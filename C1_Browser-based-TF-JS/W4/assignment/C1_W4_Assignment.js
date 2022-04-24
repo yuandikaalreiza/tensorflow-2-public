670581278fa81ef819bd1d7ffeadff118a6dd358
yuandikaalreiza <yuandikaalfahreiza@gmail.com>
2022-04-24 13:40:56 +0700

    Update C1_W4_Assignment.js
    Finished C1W2 graded lab

--- C1_Browser-based-TF-JS/W4/assignment/C1_W4_Assignment.js
+++ C1_Browser-based-TF-JS/W4/assignment/C1_W4_Assignment.js
@@ -27,18 +27,21 @@ async function train() {
     layers: [
         
       // YOUR CODE HERE
+      tf.layers.flatten({inputShape: mobilenet.outputs[0].shape.slice(1)}),
+      tf.layers.dense({units: 100, activation:'relu'}),
+      tf.layers.dense({units: 5, activation:'softmax'})
 
     ]
   });
     
    
   // Set the optimizer to be tf.train.adam() with a learning rate of 0.0001.
-  const optimizer = // YOUR CODE HERE
+  const optimizer = tf.train.adam(0.0001);// YOUR CODE HERE
     
         
   // Compile the model using the categoricalCrossentropy loss, and
   // the optimizer you defined above.
-  model.compile(// YOUR CODE HERE);
+  model.compile({optimizer: optimizer, loss: 'categoricalCrossentropy'});
  
   let loss = 0;
   model.fit(dataset.xs, dataset.ys, {
@@ -71,11 +74,14 @@ function handleButton(elem){
 			spockSamples++;
 			document.getElementById("spocksamples").innerText = "Spock samples:" + spockSamples;
 			break;
-            
         // Add a case for lizard samples.
         // HINT: Look at the previous cases.
             
         // YOUR CODE HERE
+    case "4":
+			lizardSamples++;
+			document.getElementById("lizardsamples").innerText = "Lizard samples:" + lizardSamples;
+			break;
 		
             
 	}
@@ -113,6 +119,9 @@ async function predict() {
         // HINT: Look at the previous cases.
             
         // YOUR CODE HERE 
+    case 4:
+			predictionText = "I see Lizard";
+			break;
 	
             
 	}
